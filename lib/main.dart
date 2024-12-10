b5d0db787ea3e663896853a61761d6139ce807cc
jin <mizu4356@gmail.com>
2024-12-10 22:17:12 +0900

    main.dart 表を表示・編集処理追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class User {
   final String id;
-  final String name;
-  final int age;
-  final String hobby;
+  String name;
+  int age;
+  String hobby;
 
   User({required this.id, required this.name, required this.age, required this.hobby});
 }
@@ -27,159 +27,95 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class UserTable extends StatelessWidget {
-  final List<User> users = [
+class UserTable extends StatefulWidget {
+  @override
+  _UserTableState createState() => _UserTableState();
+}
+
+class _UserTableState extends State<UserTable> {
+  List<User> users = [
     User(id: '001', name: '佐藤 太郎', age: 25, hobby: '読書'),
     User(id: '002', name: '鈴木 次郎', age: 30, hobby: '映画鑑賞'),
     User(id: '003', name: '高橋 三郎', age: 22, hobby: '音楽'),
   ];
 
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+  final TextEditingController hobbyController = TextEditingController();
+  int? selectedIndex;
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: DataTable(
-        columns: [
-          DataColumn(label: Text('ID')),
-          DataColumn(label: Text('名前')),
-          DataColumn(label: Text('年齢')),
-          DataColumn(label: Text('趣味')),
+    return Column(
+      children: [
+        SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            columns: [
+              DataColumn(label: Text('ID')),
+              DataColumn(label: Text('名前')),
+              DataColumn(label: Text('年齢')),
+              DataColumn(label: Text('趣味')),
+              DataColumn(label: Text('編集')),
+            ],
+            rows: users.map((user) {
+              int index = users.indexOf(user);
+              return DataRow(cells: [
+                DataCell(Text(user.id)),
+                DataCell(Text(user.name)),
+                DataCell(Text(user.age.toString())),
+                DataCell(Text(user.hobby)),
+                DataCell(IconButton(
+                  icon: Icon(Icons.edit),
+                  onPressed: () {
+                    setState(() {
+                      selectedIndex = index;
+                      nameController.text = user.name;
+                      ageController.text = user.age.toString();
+                      hobbyController.text = user.hobby;
+                    });
+                  },
+                )),
+              ]);
+            }).toList(),
+          ),
+        ),
+        if (selectedIndex != null) ...[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('編集'),
+                TextField(
+                  controller: nameController,
+                  decoration: InputDecoration(labelText: '名前'),
+                ),
+                TextField(
+                  controller: ageController,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(labelText: '年齢'),
+                ),
+                TextField(
+                  controller: hobbyController,
+                  decoration: InputDecoration(labelText: '趣味'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      users[selectedIndex!].name = nameController.text;
+                      users[selectedIndex!].age = int.parse(ageController.text);
+                      users[selectedIndex!].hobby = hobbyController.text;
+                      selectedIndex = null;
+                    });
+                  },
+                  child: Text('保存'),
+                ),
+              ],
+            ),
+          ),
         ],
-        rows: users.map((user) {
-          return DataRow(cells: [
-            DataCell(Text(user.id)),
-            DataCell(Text(user.name)),
-            DataCell(Text(user.age.toString())),
-            DataCell(Text(user.hobby)),
-          ]);
-        }).toList(),
-      ),
+      ],
     );
   }
 }
-
-
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // TRY THIS: Try running your application with "flutter run". You'll see
-//         // the application has a purple toolbar. Then, without quitting the app,
-//         // try changing the seedColor in the colorScheme below to Colors.green
-//         // and then invoke "hot reload" (save your changes or press the "hot
-//         // reload" button in a Flutter-supported IDE, or press "r" if you used
-//         // the command line to start the app).
-//         //
-//         // Notice that the counter didn't reset back to zero; the application
-//         // state is not lost during the reload. To reset the state, use hot
-//         // restart instead.
-//         //
-//         // This works for code too, not just values: Most code changes can be
-//         // tested with just a hot reload.
-//         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-//         useMaterial3: true,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // TRY THIS: Try changing the color here to a specific color (to
-//         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-//         // change color while the other colors stay the same.
-//         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           //
-//           // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-//           // action in the IDE, or press "p" in the console), to see the
-//           // wireframe for each widget.
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
