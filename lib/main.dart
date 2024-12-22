fc1018f21e65fc79c442ea637b8b5af9e6902ede
Jmizuki <mizu4356@gmail.com>
2024-12-22 23:51:46 +0900

    Update main.dart
    単語帳編集の表に修正

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,16 @@ class User {
   User({required this.id, required this.name, required this.age, required this.hobby});
 }
 
+class Word {
+  final String id;
+  String word;
+  String japaneseTranslation;
+  String pronunciation;
+  String phoneticSymbol;
+
+  Word({required this.id, required this.word, required this.japaneseTranslation, required this.pronunciation, required this.phoneticSymbol});
+}
+
 void main() {
   runApp(MyApp());
 }
@@ -19,29 +29,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('ユーザー情報テーブル'),
+          title: Text('単語情報テーブル'),
         ),
-        body: UserTable(),
+        body: WordsTable(),
       ),
     );
   }
 }
 
-class UserTable extends StatefulWidget {
+class WordsTable extends StatefulWidget {
   @override
-  _UserTableState createState() => _UserTableState();
+  _WordsTableState createState() => _WordsTableState();
 }
 
-class _UserTableState extends State<UserTable> {
-  List<User> users = [
-    User(id: '001', name: '佐藤 太郎', age: 25, hobby: '読書'),
-    User(id: '002', name: '鈴木 次郎', age: 30, hobby: '映画鑑賞'),
-    User(id: '003', name: '高橋 三郎', age: 22, hobby: '音楽'),
+class _WordsTableState extends State<WordsTable> {
+  List<Word> words = [
+    Word(id: '001', word: 'apple', japaneseTranslation: 'りんご', pronunciation: 'ap・ple', phoneticSymbol: 'æpl'),
+    Word(id: '002', word: 'orange', japaneseTranslation: 'オレンジ', pronunciation: 'or・ange', phoneticSymbol: 'ɔːrɪndʒ'),
+    Word(id: '003', word: 'banana', japaneseTranslation: 'バナナ', pronunciation: 'ba・nan・a', phoneticSymbol: 'bənˈænə'),
   ];
 
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController ageController = TextEditingController();
-  final TextEditingController hobbyController = TextEditingController();
+  final TextEditingController wordController = TextEditingController();
+  final TextEditingController japaneseTranslationController = TextEditingController();
+  final TextEditingController pronunciationController = TextEditingController();
+  final TextEditingController phoneticSymbolController = TextEditingController();
   int? selectedIndex;
 
   @override
@@ -53,26 +64,29 @@ class _UserTableState extends State<UserTable> {
           child: DataTable(
             columns: [
               DataColumn(label: Text('ID')),
-              DataColumn(label: Text('名前')),
-              DataColumn(label: Text('年齢')),
-              DataColumn(label: Text('趣味')),
+              DataColumn(label: Text('単語')),
+              DataColumn(label: Text('日本語訳')),
+              DataColumn(label: Text('発音')),
+              DataColumn(label: Text('発音記号')),
               DataColumn(label: Text('編集')),
             ],
-            rows: users.map((user) {
-              int index = users.indexOf(user);
+            rows: words.map((word) {
+              int index = words.indexOf(word);
               return DataRow(cells: [
-                DataCell(Text(user.id)),
-                DataCell(Text(user.name)),
-                DataCell(Text(user.age.toString())),
-                DataCell(Text(user.hobby)),
+                DataCell(Text(word.id)),
+                DataCell(Text(word.word)),
+                DataCell(Text(word.japaneseTranslation)),
+                DataCell(Text(word.pronunciation)),
+                DataCell(Text(word.phoneticSymbol)),
                 DataCell(IconButton(
                   icon: Icon(Icons.edit),
                   onPressed: () {
                     setState(() {
                       selectedIndex = index;
-                      nameController.text = user.name;
-                      ageController.text = user.age.toString();
-                      hobbyController.text = user.hobby;
+                      wordController.text = word.word;
+                      japaneseTranslationController.text = word.japaneseTranslation;
+                      pronunciationController.text = word.pronunciation;
+                      phoneticSymbolController.text = word.phoneticSymbol;
                     });
                     _showEditDialog(context, index);
                   },
@@ -90,23 +104,27 @@ class _UserTableState extends State<UserTable> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('ユーザー情報編集'),
+          title: Text('単語情報編集'),
           content: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 TextField(
-                  controller: nameController,
-                  decoration: InputDecoration(labelText: '名前'),
+                  controller: wordController,
+                  decoration: InputDecoration(labelText: '単語'),
                 ),
                 TextField(
-                  controller: ageController,
+                  controller: japaneseTranslationController,
                   keyboardType: TextInputType.number,
-                  decoration: InputDecoration(labelText: '年齢'),
+                  decoration: InputDecoration(labelText: '日本語訳'),
                 ),
                 TextField(
-                  controller: hobbyController,
-                  decoration: InputDecoration(labelText: '趣味'),
+                  controller: pronunciationController,
+                  decoration: InputDecoration(labelText: '発音'),
+                ),
+                TextField(
+                  controller: phoneticSymbolController,
+                  decoration: InputDecoration(labelText: '発音記号'),
                 ),
               ],
             ),
@@ -121,9 +139,10 @@ class _UserTableState extends State<UserTable> {
             ElevatedButton(
               onPressed: () {
                 setState(() {
-                  users[index].name = nameController.text;
-                  users[index].age = int.parse(ageController.text);
-                  users[index].hobby = hobbyController.text;
+                  words[index].word = wordController.text;
+                  words[index].japaneseTranslation = japaneseTranslationController.text;
+                  words[index].pronunciation = pronunciationController.text;
+                  words[index].phoneticSymbol = phoneticSymbolController.text;
                 });
                 Navigator.of(context).pop(); // ダイアログを閉じる
               },
